d5c1c837fe0813dfd861a269388690b6966afd5e
Adnan Jakati <jackady@mac.mynetworksettings.com>
2024-10-01 06:08:07 -0400

    Assignment 1

---
+++ extract.py
@@ -0,0 +1,8 @@
+import base64, requests, json
+
+api = 'https://rekor.sigstore.dev/api/v1/log/entries?logIndex=13267353'
+
+data = requests.get(api).json()
+body = next(iter(data.values()))['body']
+body = json.loads(base64.b64decode(body).decode())
+print(body)
